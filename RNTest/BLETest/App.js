ec8eee953c1c71ab228fb82f4898f5c471d64681
lorenpap <57562403+lorenpap@users.noreply.github.com>
2020-01-08 13:39:09 +0200

    added storage, buttons for contacts and settings input

--- RNTest/BLETest/App.js
+++ RNTest/BLETest/App.js
@@ -1,6 +1,7 @@
 
 import React, {useState} from 'react';
 import {
+    View,
     SafeAreaView,
     Button,
     StatusBar,
@@ -14,7 +15,7 @@ import {
 import {BleManager} from 'react-native-ble-plx';
 import {NativeModules} from 'react-native';
 import { Component } from 'react';
-//import "storage-service" from services;
+import Storage from './services/storage-service'
 const DirectSms = NativeModules.DirectSms;
 
 const BLEManager = new BleManager();
@@ -22,11 +23,10 @@ const BLEManager = new BleManager();
 const charUUID = '0000fff4-0000-1000-8000-00805f9b34fb';
 
 
-
 const App: () => React$Node = () => {
     const [foundMyDevice, setFoundMyDevice] = useState(false);
     const [char, setChar] = useState('');
-
+    const [value, setValue] = useState('');
     const addDevice = (err, device) => {
         if (err) {
             Alert.alert('Error', err.message);
@@ -71,19 +71,8 @@ const App: () => React$Node = () => {
 
     };
 
-    function ContactsTextInput() {
-        const [value, onChangeText] = React.useState('contacts text input');}
-    
+ 
 
-    function ContactsTextInput(props) {
-        return (
-          <TextInput
-            {...props} // Inherit any props passed to it; e.g., multiline, numberOfLines below
-            editable
-            maxLength={15}
-          />
-        );
-      }
       
 
     const startScan = async () => {
@@ -105,11 +94,15 @@ const App: () => React$Node = () => {
             <SafeAreaView
                 style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
                 <Button title={'Scan'} onPress={startScan}/>
-                <ContactsTextInput
-                    onChangeText={text => onChangeText(text)}
+                <TextInput
+                    style={{width:300, height:50, borderWidth:1}}
+                    onChangeText={setValue}
                     value={value}       
                 />
-                {foundMyDevice && <Text>Yay, I found LRRM</Text>}
+                <View style={{height:100, justifyContent: 'space-between'}}>
+                <Button title={'Add contact'} onPress={() =>Storage.set_contact(value)}/>
+                <Button title={'Get contacts'} onPress={() => Storage.get_contacts().then(res => console.warn(res))}/>
+                </View>
                 <Text>Tracked char value is {char}</Text>
                 <Text>Number to send to: +972502733733!</Text>
             </SafeAreaView>
--- RNTest/BLETest/services/storage-service.js
+++ RNTest/BLETest/services/storage-service.js
@@ -2,26 +2,42 @@ import {AsyncStorage} from 'react-native';
 
 export default class StorageService{
 
-    static _storeData = async (data) => {
+    static _storeData = async (key,data) => {
         try {
-          await AsyncStorage.setItem("1",data);
+          await AsyncStorage.setItem(key,JSON.stringify(data));
         } catch (err) {
             console.error('Error in store data', err.message);
         }
       };
 
-    static _retrieveData = async () => {
+    static _retrieveData = async (key) => {
         try {
-          const value = await AsyncStorage.getItem('CONTACTS');
+          const value = JSON.parse(await AsyncStorage.getItem(key));
           if (value !== null) {
             // We have data!!
             console.warn(value);
+            return value;
           }
         } catch (err) {
             console.error('Error in retrieve data', err.message);
         }
       };
 
+    static set_contact=  async(contacts)=>{
+      await  this._storeData("CONTACTS", contacts);
+    }
+
+    static get_contacts = async()=>{
+      return  await this._retrieveData("CONTACTS");
+    }
+
+    static set_settings = async(settings)=>{
+      await this._storeData("SETTINGS", settings);
+    }
+
+    static get_settings = async()=>{
+      return await this._retrieveData("SETTINGS");
+    }
 
 
 }
