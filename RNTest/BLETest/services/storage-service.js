7aef14c04391f65450f72a9a25910c67f076f914
Ron Belkin <ronbel249@gmail.com>
2020-01-13 00:14:25 +0200

    Added a working settings screen

--- RNTest/BLETest/services/storage-service.js
+++ RNTest/BLETest/services/storage-service.js
@@ -1,41 +1,47 @@
 import {AsyncStorage} from 'react-native';
 
-export default class StorageService{
+const DEFAULT_SETTINGS = {
+    message: 'Help me, I am in trouble!',
+    addLocation: false,
+    secondsToWait: 7,
+};
 
-    static _storeData = async (key,data) => {
+export default class StorageService {
+
+    static _storeData = async (key, data) => {
         try {
-          await AsyncStorage.setItem(key,JSON.stringify(data));
+            await AsyncStorage.setItem(key, JSON.stringify(data));
         } catch (err) {
             console.error('Error in store data', err.message);
         }
-      };
+    };
 
     static _retrieveData = async (key) => {
         try {
-          const value = JSON.parse(await AsyncStorage.getItem(key));
-          if (value !== null) {
-            return value;
-          }
+            const value = JSON.parse(await AsyncStorage.getItem(key));
+            if (value !== null) {
+                return value;
+            }
         } catch (err) {
             console.error('Error in retrieve data', err.message);
         }
-      };
-
-    static set_contact=  async(contacts)=>{
-      await  this._storeData("CONTACTS", contacts);
     };
 
-    static get_contacts = async()=>{
-      return  await this._retrieveData("CONTACTS");
+    static set_contact = async (contacts) => {
+        await this._storeData('CONTACTS', contacts);
     };
 
-    static set_settings = async(settings)=>{
-      await this._storeData("SETTINGS", settings);
-    }
+    static get_contacts = async () => {
+        return await this._retrieveData('CONTACTS');
+    };
 
-    static get_settings = async()=>{
-      return await this._retrieveData("SETTINGS");
-    }
+    static set_settings = async (settings) => {
+        await this._storeData('SETTINGS', settings);
+    };
+
+    static get_settings = async () => {
+        return (await this._retrieveData('SETTINGS')) || DEFAULT_SETTINGS;
+    };
 
 
 }
